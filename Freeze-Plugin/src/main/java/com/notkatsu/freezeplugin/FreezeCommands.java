5a8e5663a54f2fae3940fc17f79cedd6b277cceb
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-01 11:46:57 +0100

    Add checks to check if player exists

--- Freeze-Plugin/src/main/java/com/notkatsu/freezeplugin/FreezeCommands.java
+++ Freeze-Plugin/src/main/java/com/notkatsu/freezeplugin/FreezeCommands.java
@@ -1,16 +1,42 @@
 package com.notkatsu.freezeplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class FreezeCommands implements CommandExecutor {
+
+    public List<UUID> frozenPlayers = new ArrayList<>();
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            
+            Player command_author = (Player) commandSender;
+
+            if (strings.length == 1) {
+                Player player = Bukkit.getPlayer(strings[0]);
+
+                if (player == null) {
+                    command_author.sendMessage(ChatColor.RED + " that player does not exist.. Please try again.");
+
+                } else {
+                    if (command_author.isOp()) {
+                        if frozenPlayers.contains(player.)
+                    } else {
+                        command_author.sendMessage(ChatColor.RED + player.getDisplayName() + ChatColor.WHITE + " only Players with OP can run this command..");
+                    }
+                }
+
+            }
+        } else {
+            System.out.println("/freeze can only be used by Players not the Console.");
         }
         return false;
     }
