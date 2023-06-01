78f03bd706e1819501ed3a6eba460fbc6c044d08
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-01 11:35:28 +0100

    Create FreezeCommands.java

--- Freeze-Plugin/src/main/java/com/notkatsu/freezeplugin/FreezeCommands.java
+++ Freeze-Plugin/src/main/java/com/notkatsu/freezeplugin/FreezeCommands.java
@@ -1,4 +1,12 @@
 package com.notkatsu.freezeplugin;
 
-public class FreezeCommands {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class FreezeCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
 }
