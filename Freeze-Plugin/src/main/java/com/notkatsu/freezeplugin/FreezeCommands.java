67bbbd7a26ffc8cca7d3aa3ad0f0c52aa357183f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-01 11:40:11 +0100

    Check if commandSender is an instance of Player

--- Freeze-Plugin/src/main/java/com/notkatsu/freezeplugin/FreezeCommands.java
+++ Freeze-Plugin/src/main/java/com/notkatsu/freezeplugin/FreezeCommands.java
@@ -3,10 +3,15 @@ package com.notkatsu.freezeplugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class FreezeCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            
+        }
         return false;
     }
 }
