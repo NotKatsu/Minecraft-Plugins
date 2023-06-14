c4b65455c888d446b9bad4b5e5cf671693b6ccab
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-06-14 21:47:43 +0100

    Create Entities-Plugin

---
+++ Entities-Plugin/src/main/java/com/notkatsu/entitiesplugin/Entities_Plugin.java
@@ -0,0 +1,11 @@
+package com.notkatsu.entitiesplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Entities_Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand()
+    }
+}
